8b08f0c4512f61a56a16c08841323c8e96ea45a3
Morgan Christiansson <git@mog.se>
2025-03-25 09:51:46 +0100

    Add new pentapod enemies for Atrixsanna

--- prototypes/enemies-base.lua
+++ prototypes/enemies-base.lua
@@ -1,4 +1,4 @@
-local titan_biter_scale = 2
+local titan_biter_scale = 1.7
 local titan_biter_tint1 = {0.2, 0.25, 0.2, 1}
 local titan_biter_tint2 = {0.82, 0.45, 0.1, 0.7}
 
@@ -11,17 +11,41 @@ titan_biter.water_reflection = biter_water_reflection(titan_biter_scale)
 
 
 
-local scale_spitter_titan = 1.2
+local gargantuan_biter_scale = 2
+local gargantuan_biter_tint1 = {0.2, 0.25, 0.2, 1}
+local gargantuan_biter_tint2 = {0.82, 0.45, 0.1, 0.7}
+
+local gargantuan_biter = table.deepcopy(data.raw.unit['behemoth-biter'])
+gargantuan_biter.name = 'gargantuan-biter'
+gargantuan_biter.max_health = 300000
+gargantuan_biter.attack_parameters.animation = biterattackanimation(gargantuan_biter_scale, gargantuan_biter_tint1, gargantuan_biter_tint2)
+gargantuan_biter.run_animation = biterrunanimation(gargantuan_biter_scale, gargantuan_biter_tint1, gargantuan_biter_tint2)
+gargantuan_biter.water_reflection = biter_water_reflection(gargantuan_biter_scale)
+
+
+
+local scale_spitter_titan = 1.7
 local tint_1_spitter_titan = {0.3, 0.2, 0.06, 1}
 local tint_2_spitter_titan = {0.7, 0.45, 0.12, 0.75}
 
 local titan_spitter = table.deepcopy(data.raw.unit['behemoth-spitter'])
 titan_spitter.name = 'titan-spitter'
-titan_spitter.max_health = 30000
--- print(serpent.block(titan_spitter.attack_parameters))
+titan_spitter.max_health = 15000
 titan_spitter.run_animation = spitterrunanimation(scale_spitter_titan, tint_1_spitter_titan, tint_2_spitter_titan)
 titan_spitter.water_reflection = spitter_water_reflection(scale_spitter_titan)
 
 
 
-data:extend{ titan_biter, titan_spitter, titan_stomper, titan_strafer }
+local scale_spitter_gargantuan = 2
+local tint_1_spitter_gargantuan = {0.3, 0.2, 0.06, 1}
+local tint_2_spitter_gargantuan = {0.7, 0.45, 0.12, 0.75}
+
+local gargantuan_spitter = table.deepcopy(data.raw.unit['behemoth-spitter'])
+gargantuan_spitter.name = 'gargantuan-spitter'
+gargantuan_spitter.max_health = 150000
+gargantuan_spitter.run_animation = spitterrunanimation(scale_spitter_gargantuan, tint_1_spitter_gargantuan, tint_2_spitter_gargantuan)
+gargantuan_spitter.water_reflection = spitter_water_reflection(scale_spitter_gargantuan)
+
+
+
+data:extend{ titan_biter, gargantuan_biter, titan_spitter, gargantuan_spitter }
--- prototypes/enemies-space-age.lua
+++ prototypes/enemies-space-age.lua
@@ -19,11 +19,47 @@ local function grey_overlay(tint, amount) -- fades to opaque grey. Full opacity 
   return lerp_color(tint, {127, 127, 127, 255}, amount)
 end
 
+local gleba_behemoth_mask_tint  = {216,0,35,255}
+local gleba_behemoth_mask2_tint  = {216,100,35,255}
+local gleba_behemoth_body_tint = {117,116,104,255}
+local gleba_behemoth_scale = 1.2
+
 local gleba_titan_mask_tint  = {216,0,35,255}
 local gleba_titan_mask2_tint  = {216,100,35,255}
 local gleba_titan_body_tint = {117,116,104,255}
-local gleba_titan_scale = 2
-make_strafer("titan-", gleba_titan_scale, 30000, 1.6, 5.5, 26, 31, 36,
+local gleba_titan_scale = 1.7
+
+local gleba_gargantuan_mask_tint  = {216,0,35,255}
+local gleba_gargantuan_mask2_tint  = {216,100,35,255}
+local gleba_gargantuan_body_tint = {117,116,104,255}
+local gleba_gargantuan_scale = 2
+
+make_wriggler("behemoth-", gleba_behemoth_scale, 4500, 1.8,
+  {
+    mask = fade(gleba_behemoth_mask_tint, 0.5),
+    body = gleba_behemoth_body_tint
+  }, simulations.factoriopedia_gleba_enemy_big_wriggler, simulations.factoriopedia_gleba_enemy_big_wriggler_premature, space_age_sounds.wriggler_pentapod.big)
+make_wriggler("titan-", gleba_titan_scale, 45000, 1.8,
+  {
+    mask = fade(gleba_titan_mask_tint, 0.5),
+    body = gleba_titan_body_tint
+  }, simulations.factoriopedia_gleba_enemy_big_wriggler, simulations.factoriopedia_gleba_enemy_big_wriggler_premature, space_age_sounds.wriggler_pentapod.big)
+make_wriggler("gargantuan-", gleba_gargantuan_scale, 450000, 1.8,
+  {
+    mask = fade(gleba_gargantuan_mask_tint, 0.5),
+    body = gleba_gargantuan_body_tint
+  }, simulations.factoriopedia_gleba_enemy_big_wriggler, simulations.factoriopedia_gleba_enemy_big_wriggler_premature, space_age_sounds.wriggler_pentapod.big)
+
+
+make_strafer("behemoth-", gleba_behemoth_scale, 20000, 1.6, 5.5, 26, 31, 36,
+  {
+    mask = fade(gleba_behemoth_mask_tint, 0.4),
+    mask_thigh = fade(gleba_behemoth_mask2_tint, 0.2),
+    body = grey_overlay(gleba_behemoth_body_tint, 0.1),
+    projectile_mask = big_wriggler_mask_tint, -- same as wriggler mask tint
+    projectile = big_wriggler_body_tint  -- same as wriggler body tint
+  }, simulations.factoriopedia_gleba_enemy_big_strafer, space_age_sounds.strafer_pentapod.big)
+make_strafer("titan-", gleba_titan_scale, 200000, 1.6, 5.5, 26, 31, 36,
   {
     mask = fade(gleba_titan_mask_tint, 0.4),
     mask_thigh = fade(gleba_titan_mask2_tint, 0.2),
@@ -31,29 +67,49 @@ make_strafer("titan-", gleba_titan_scale, 30000, 1.6, 5.5, 26, 31, 36,
     projectile_mask = big_wriggler_mask_tint, -- same as wriggler mask tint
     projectile = big_wriggler_body_tint  -- same as wriggler body tint
   }, simulations.factoriopedia_gleba_enemy_big_strafer, space_age_sounds.strafer_pentapod.big)
-make_stomper("titan-", gleba_titan_scale, 30000, 1.6, 2.8,
+
+
+make_stomper("behemoth-", gleba_behemoth_scale, 120000, 1.6, 2.8,
+  {
+    mask = fade(gleba_behemoth_mask_tint, 0.4),
+    mask_thigh = fade(gleba_behemoth_mask2_tint, 0.3),
+    body = grey_overlay(gleba_behemoth_body_tint, 0.1),
+    body_thigh = lerp_color(gleba_behemoth_body_tint, grey_overlay({250,108,0,255}, 0.7), 0.1) -- more orange/yellow
+  }, simulations.factoriopedia_gleba_enemy_big_stomper, space_age_sounds.stomper_pentapod.big)
+make_stomper("titan-", gleba_titan_scale, 1000000, 1.6, 2.8,
   {
     mask = fade(gleba_titan_mask_tint, 0.4),
     mask_thigh = fade(gleba_titan_mask2_tint, 0.3),
     body = grey_overlay(gleba_titan_body_tint, 0.1),
     body_thigh = lerp_color(gleba_titan_body_tint, grey_overlay({250,108,0,255}, 0.7), 0.1) -- more orange/yellow
   }, simulations.factoriopedia_gleba_enemy_big_stomper, space_age_sounds.stomper_pentapod.big)
-make_wriggler("titan-", gleba_titan_scale, 4000, 1.8,
-  {
-    mask = fade(gleba_titan_mask_tint, 0.5),
-    body = gleba_titan_body_tint
-  }, simulations.factoriopedia_gleba_enemy_big_wriggler, simulations.factoriopedia_gleba_enemy_big_wriggler_premature, space_age_sounds.wriggler_pentapod.big)
 
-local k = 'titan'
--- data.raw["segmented-unit"][k.."-demolisher"].icon = "__space-age__/graphics/icons/big-demolisher.png"
--- data.raw["segmented-unit"][k.."-demolisher"].vision_distance = math.min(100, data.raw["segmented-unit"][k.."-demolisher"].vision_distance)
-data.raw["spider-unit"][k.."-strafer-pentapod"].icon = "__space-age__/graphics/icons/big-strafer.png"
-data.raw["spider-unit"][k.."-stomper-pentapod"].icon = "__space-age__/graphics/icons/big-stomper.png"
-data.raw["simple-entity"][k.."-stomper-shell"].icon = "__space-age__/graphics/icons/big-stomper.png"
-data.raw["unit"][k.."-wriggler-pentapod-premature"].icon = "__space-age__/graphics/icons/big-wriggler.png"
-data.raw["unit"][k.."-wriggler-pentapod"].icon = "__space-age__/graphics/icons/big-wriggler.png"
-data.raw["corpse"][k .. "-wriggler-pentapod-corpse"].icon = "__space-age__/graphics/icons/big-wriggler-corpse.png"
-data.raw["corpse"][k .. "-stomper-corpse"].icon = "__space-age__/graphics/icons/big-stomper.png"
-data.raw["corpse"][k .. "-strafer-corpse"].icon = "__space-age__/graphics/icons/big-strafer.png"
-data.raw["spider-leg"][k .. "-stomper-pentapod-leg"].icon = "__space-age__/graphics/icons/big-stomper.png"
-data.raw["spider-leg"][k .. "-strafer-pentapod-leg"].icon = "__space-age__/graphics/icons/big-strafer.png"
+
+-- data.raw["segmented-unit"]["titan-demolisher"].icon = "__space-age__/graphics/icons/big-demolisher.png"
+-- data.raw["segmented-unit"]["titan-demolisher"].vision_distance = math.min(100, data.raw["segmented-unit"]["titan-demolisher"].vision_distance)
+
+data.raw["spider-unit"]["behemoth-strafer-pentapod"].icon = "__space-age__/graphics/icons/big-strafer.png"
+data.raw["spider-unit"]["behemoth-stomper-pentapod"].icon = "__space-age__/graphics/icons/big-stomper.png"
+data.raw["simple-entity"]["behemoth-stomper-shell"].icon = "__space-age__/graphics/icons/big-stomper.png"
+data.raw["unit"]["behemoth-wriggler-pentapod-premature"].icon = "__space-age__/graphics/icons/big-wriggler.png"
+data.raw["unit"]["behemoth-wriggler-pentapod"].icon = "__space-age__/graphics/icons/big-wriggler.png"
+data.raw["corpse"]["behemoth-wriggler-pentapod-corpse"].icon = "__space-age__/graphics/icons/big-wriggler-corpse.png"
+data.raw["corpse"]["behemoth-stomper-corpse"].icon = "__space-age__/graphics/icons/big-stomper.png"
+data.raw["corpse"]["behemoth-strafer-corpse"].icon = "__space-age__/graphics/icons/big-strafer.png"
+data.raw["spider-leg"]["behemoth-stomper-pentapod-leg"].icon = "__space-age__/graphics/icons/big-stomper.png"
+data.raw["spider-leg"]["behemoth-strafer-pentapod-leg"].icon = "__space-age__/graphics/icons/big-strafer.png"
+
+data.raw["spider-unit"]["titan-strafer-pentapod"].icon = "__space-age__/graphics/icons/big-strafer.png"
+data.raw["spider-unit"]["titan-stomper-pentapod"].icon = "__space-age__/graphics/icons/big-stomper.png"
+data.raw["simple-entity"]["titan-stomper-shell"].icon = "__space-age__/graphics/icons/big-stomper.png"
+data.raw["unit"]["titan-wriggler-pentapod-premature"].icon = "__space-age__/graphics/icons/big-wriggler.png"
+data.raw["unit"]["titan-wriggler-pentapod"].icon = "__space-age__/graphics/icons/big-wriggler.png"
+data.raw["corpse"]["titan-wriggler-pentapod-corpse"].icon = "__space-age__/graphics/icons/big-wriggler-corpse.png"
+data.raw["corpse"]["titan-stomper-corpse"].icon = "__space-age__/graphics/icons/big-stomper.png"
+data.raw["corpse"]["titan-strafer-corpse"].icon = "__space-age__/graphics/icons/big-strafer.png"
+data.raw["spider-leg"]["titan-stomper-pentapod-leg"].icon = "__space-age__/graphics/icons/big-stomper.png"
+data.raw["spider-leg"]["titan-strafer-pentapod-leg"].icon = "__space-age__/graphics/icons/big-strafer.png"
+
+data.raw["unit"]["gargantuan-wriggler-pentapod-premature"].icon = "__space-age__/graphics/icons/big-wriggler.png"
+data.raw["unit"]["gargantuan-wriggler-pentapod"].icon = "__space-age__/graphics/icons/big-wriggler.png"
+data.raw["corpse"]["gargantuan-wriggler-pentapod-corpse"].icon = "__space-age__/graphics/icons/big-wriggler-corpse.png"
